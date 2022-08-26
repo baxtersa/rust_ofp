f0e0867a39b759dda23cc9155182cbe4829386a6
Banyc <36535895+Banyc@users.noreply.github.com>
2022-08-26 15:59:38 +0800

    fix: remove deprecated use of anonymous parameters

--- src/ofp_message.rs
+++ src/ofp_message.rs
@@ -5,12 +5,12 @@ use ofp_header::OfpHeader;
 /// Version-agnostic API for handling OpenFlow messages at the byte-buffer level.
 pub trait OfpMessage {
     /// Return the byte-size of an `OfpMessage`.
-    fn size_of(&Self) -> usize;
+    fn size_of(msg: &Self) -> usize;
     /// Create an `OfpHeader` for the given transaction id and OpenFlow message.
-    fn header_of(u32, &Self) -> OfpHeader;
+    fn header_of(xid: u32, msg: &Self) -> OfpHeader;
     /// Return a marshaled buffer containing an OpenFlow header and the message `msg`.
-    fn marshal(u32, Self) -> Vec<u8>;
+    fn marshal(xid: u32, msg: Self) -> Vec<u8>;
     /// Returns a pair `(u32, OfpMessage)` of the transaction id and OpenFlow message parsed from
     /// the given OpenFlow header `header`, and buffer `buf`.
-    fn parse(&OfpHeader, &[u8]) -> (u32, Self);
+    fn parse(header: &OfpHeader, buf: &[u8]) -> (u32, Self);
 }
